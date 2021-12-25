5dfcaea8ef37f8f958ce3f249614f0b9f6370b62
aincatoni <ain.catoni@gmail.com>
2021-12-25 18:07:52 -0300

    agrega seción de iconos sociales y darkmode via css

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -3,15 +3,17 @@ import React from "react";
 function Bio({ ImageCard, TituloCard, BajadaCard }) {
   return (
     <div
-      className="p-4 w-full transition-all 
+      className="dark:bg-zinc-700 dark:text-gray-400 p-4 w-full transition-all 
     mx-auto bg-white shadow-md flex items-center space-x-11 mb-5"
     >
       <div className="w-36">
         <img className="rounded-full" src={ImageCard} alt="" />
       </div>
 
-      <div className="w-full md:w-60">
-        <h2 className="text-gray-800 font-bold">{TituloCard}</h2>
+      <div className="w-full">
+        <h2 className="dark:text-gray-500 text-gray-800 font-bold">
+          {TituloCard}
+        </h2>
         <h6 className="text-gray-400">{BajadaCard}</h6>
       </div>
     </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ ImageCard, EnlaceCard, TituloCard }) {
   return (
-    <div className="pr-2 w-full transition-all mx-auto bg-white rounded-xl shadow-md hover:shadow-lg flex items-center space-x-5 md:space-x-11 my-5 md:my-0">
+    <div className="dark:bg-zinc-700 dark:text-gray-400 pr-2 w-full transition-all mx-auto bg-white rounded-xl shadow-md hover:shadow-lg flex items-center space-x-5 md:space-x-11 my-5 md:my-0">
       <div className="w-20 md:w-36">
         <img className="rounded-l-lg object-fit" src={ImageCard} alt="" />
       </div>
